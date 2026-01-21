4cb967c10becc6790e5df2d709d5f2a78fd04e4c
Antonio Luca Pirrone <antoniolucapirrone06@gmail.com>
2026-01-21 02:37:02 +0100

    Note e fix dei livelli - ora perfettamente funzionali, ma inesplorabili senza bombe

--- src/Levels.cpp
+++ src/Levels.cpp
@@ -48,7 +48,7 @@ map Levels::genlevels() {  //questa funzione genera i 5 livelli e ritorna un arr
             }
 
     for (int liv=0; liv<5; liv++) {
-        if (liv !=0) true_map.level[2][1][liv] = char(174);
+        if (liv !=0) true_map.level[1][1][liv] = char(174);
         if (liv !=4) true_map.level[21][84][liv] = char(175);
     }
     return true_map;
@@ -63,17 +63,22 @@ WINDOW* Levels::enclose_screen(map* map) {  //questa funzione mostra su schermo 
     int x_offset = getmaxx(stdscr) / 2 - 43;
     if (x_offset < 0) x_offset = 0;
 
-    WINDOW * screen = newwin(23, 86, 5, x_offset);
+    WINDOW * screen = newwin(23, 86, 3, x_offset);
 
     for (int y=0; y<23; y++)
-        for(int x=0; x<86; x+=2) {
+        for(int x=0; x<86; x++) {
             mvwprintw(screen, y, x, "%c", map->level[y][x][0]);
         }
 
-    wborder(screen,  ACS_VLINE, ACS_VLINE,
+    /*wborder(screen,  ACS_VLINE, ACS_VLINE,
                  ACS_HLINE, ACS_HLINE,
                  ACS_ULCORNER, ACS_URCORNER,
-                 ACS_LLCORNER, ACS_LRCORNER);
+                 ACS_LLCORNER, ACS_LRCORNER);*/
+
+    /*wborder(screen,  186, 186,
+                     205, 205,
+                     201, 187,
+                     200, 188);*/
 
     wrefresh(screen);
 
@@ -91,14 +96,14 @@ int Levels::change_level(map *map, WINDOW* screen, bool action, int lvl) { //Man
     else if (!action && lvl>0) lvl--; // ATTENZIONE: si suppone che il primo lvl passato sia 0 dopo la chiamata di enclose_screen
 
     for (int y=0; y<23; y++)
-        for(int x=0; x<86; x+=2) {
+        for(int x=0; x<86; x++) {
             mvwprintw(screen, y, x, "%c", map->level[y][x][lvl]);
         }
 
-    wborder(screen,  ACS_VLINE, ACS_VLINE,
+    /*wborder(screen,  ACS_VLINE, ACS_VLINE,
                  ACS_HLINE, ACS_HLINE,
                  ACS_ULCORNER, ACS_URCORNER,
-                 ACS_LLCORNER, ACS_LRCORNER);
+                 ACS_LLCORNER, ACS_LRCORNER);*/
 
     wrefresh(screen);
 
@@ -112,6 +117,7 @@ int Levels::change_level(map *map, WINDOW* screen, bool action, int lvl) { //Man
 
 void Levels::run() {
     initscr();
+    clear();
     Map = genlevels();
     WINDOW* screen = enclose_screen(&Map);
     keypad(screen, true);
@@ -145,7 +151,7 @@ void Levels::run() {
             case 'a':
             case 'A':
             case KEY_LEFT:
-                if (p.getX()>2 && Map.level[p.getY()][p.getX()-1][lvl] != char(219) && Map.level[p.getY()][p.getX()-1][lvl] != char(177)) {
+                if (/*p.getX()>2 &&*/ Map.level[p.getY()][p.getX()-1][lvl] != char(219) && Map.level[p.getY()][p.getX()-1][lvl] != char(177)) {
                     p.move(0, -1);
                     mvwprintw(screen, p.getY(), p.getX(), "%c", p.getSymbol());
                     mvwprintw(screen, p.getY(), p.getX()+1, "%c", Map.level[p.getY()][p.getX()+1][lvl]);
@@ -156,7 +162,7 @@ void Levels::run() {
             case 's':
             case 'S':
             case KEY_DOWN:
-                if (p.getY()<22 && Map.level[p.getY()+1][p.getX()][lvl] != char(219) && Map.level[p.getY()+1][p.getX()][lvl] != char(177)) {
+                if (/*p.getY()<22 &&*/ Map.level[p.getY()+1][p.getX()][lvl] != char(219) && Map.level[p.getY()+1][p.getX()][lvl] != char(177)) {
                     p.move(1, 0);
                     mvwprintw(screen, p.getY(), p.getX(), "%c", p.getSymbol());
                     mvwprintw(screen, p.getY()-1, p.getX(), "%c", Map.level[p.getY()-1][p.getX()][lvl]);
@@ -167,7 +173,7 @@ void Levels::run() {
             case 'd':
             case 'D':
             case KEY_RIGHT:
-                if (p.getX()<84 && Map.level[p.getY()][p.getX()+1][lvl] != char(219) && Map.level[p.getY()][p.getX()+1][lvl] != char(177)) {
+                if (/*p.getX()<84 &&*/ Map.level[p.getY()][p.getX()+1][lvl] != char(219) && Map.level[p.getY()][p.getX()+1][lvl] != char(177)) {
                     p.move(0, 1);
                     mvwprintw(screen, p.getY(), p.getX(), "%c", p.getSymbol());
                     mvwprintw(screen, p.getY(), p.getX()-1, "%c", Map.level[p.getY()][p.getX()-1][lvl]);
@@ -179,12 +185,23 @@ void Levels::run() {
                 ingame = false;
                 break;
 
+            /*case 'p':
+            case 'P':
+                int x_offset = getmaxx(stdscr) / 2 - 43;
+                if (x_offset < 0) x_offset = 0;
+                WINDOW* controls = newwin(23, 86, 3, x_offset);
+                mvwprintw(controls,1,1, "> Comandi del gioco: < \n\n");
+                wprintw(controls, " > WASD / keypad: movimento \n> E: piazza mina \n> ESC: esci \n\n Premi P o ESC per uscire.");
+                box(controls, 0, 0);
+                do {wrefresh(controls);}while(getch() != 27 && getch()!= 'P');
+                delwin(controls);
+                refresh();*/
         }
 
         if (moved==true) {
             if (Map.level[p.getY()][p.getX()][lvl]==char(174)) {
                 lvl = change_level(&Map, screen, 0, lvl); //cambi di livello
-                p.setposition(1, 2);
+                p.setposition(21, 83);
                 mvwprintw(screen, p.getY(), p.getX(), "%c", p.getSymbol());
             }
             else if (Map.level[p.getY()][p.getX()][lvl]==char(175)) {

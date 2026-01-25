b579ad1a8ae15683495d7f578d205b99d573582d
Gatto <manuel.sgarbi@studio.unibo.it>
2026-01-25 22:08:13 +0100

    Update

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,27 +1,38 @@
-#include "../include/Enemy.hpp"
+#include "../include/Enemy.h"
 #include <cstdlib>
 
 Enemy::Enemy(int startY, int startX, int spd, char sym) {
-    x = startX;
     y = startY;
+    x = startX;
     speed = spd;
     symbol = sym;
     alive = true;
 }
 
 void Enemy::moveRandom() {
-    int dir = rand() % 4;
-    if (dir == 0) x += speed;
-    if (dir == 1) x -= speed;
-    if (dir == 2) y += speed;
-    if (dir == 3) y -= speed;
+    int r = rand() % 4;
+    if (r == 0) y -= speed;
+    if (r == 1) y += speed;
+    if (r == 2) x -= speed;
+    if (r == 3) x += speed;
 }
 
 void Enemy::kill() {
     alive = false;
 }
 
-int Enemy::getX() const { return x; }
-int Enemy::getY() const { return y; }
-char Enemy::getSymbol() const { return symbol; }
-bool Enemy::isAlive() const { return alive; }
+int Enemy::getX() {
+    return x;
+}
+
+int Enemy::getY() {
+    return y;
+}
+
+char Enemy::getSymbol() {
+    return symbol;
+}
+
+bool Enemy::isAlive() {
+    return alive;
+}
